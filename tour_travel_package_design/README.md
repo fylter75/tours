f285c52af315b83949a1386b4f426f2fef43b15e
Rajan-SerpentCS <rajan.p.serpentcs@gmail.com>
2022-12-27 10:40:08 +0530

    Remove Code

--- tour_travel_package_design/README.md
+++
@@ -1,6 +0,0 @@
-Tour Travel Package
----------------
-
-Serpentcs <a href="https://www.serpentcs.com">
-
-
--- tour_travel_package_design/report/__init__.py
+++
@@ -1,8 +0,0 @@
-#  -*- coding: utf-8 -*-
-#  Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import qweb_package_report
-from . import invoice_package_report
-from . import qweb_po_package_report
-from . import booking_ref_room_report
-from . import rfq_quote_report
